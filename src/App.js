76202cb87af982f0a8f8440155146b7d691d0979
AnkushRai-222 <ankushrai222@gmail.com>
2023-01-05 12:17:24 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
-import Form from "./Form"
+import React, { useState } from "react";
+import './App.css'
+import Button from './Components/Button';
  
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <Form />
+    <div className="app">
+      <div>
+        <div className="count">
+          <h3>Counter:</h3>
+          <h1>{count}</h1>
+        </div>
+        <div className="buttons">
+          <Button title={"+IncrementCount"} action={incrementCount} />
+          <Button title={"-DecrementCount"} action={decrementCount} />
+        </div>
+      </div>
     </div>
-     
   );
 }
- 
-export default App;
